095f16577aaf697369c15071816c9a367023107b
remi <remi.vieux@gmail.com>
2025-09-30 17:34:50 +0200

    upd: package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
