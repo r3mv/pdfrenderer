6f381b952d8460f311c1e565d650839e62b73277
remi <remi.vieux@gmail.com>
2025-09-30 16:51:11 +0200

    ajout messages de logs

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json({limit: '5mb'})); // support HTML volumineux
 app.post('/generate-pdf', async (req, res) => {
     const auth = req.headers.authorization;
     if (!auth || auth !== `Bearer ${SECRET}`) {
+        console.log(`[PDF SERVICE] Tentative avec token invalide : ${auth}`);
         return res.status(401).send({error: "Unauthorized"});
     }
     const {html} = req.body;
@@ -23,10 +24,12 @@ app.post('/generate-pdf', async (req, res) => {
         const pdfBuffer = await page.pdf({format: 'A4'});
         await browser.close();
 
+        console.log(`[PDF SERVICE] PDF généré avec succès pour la requête.`);
         res.contentType('application/pdf');
         res.send(pdfBuffer);
     } catch (err) {
         console.error(err);
+        console.log(`[PDF SERVICE] Une erreur est survenue.`, err);
         res.status(500).send({error: err.message});
     }
 });
