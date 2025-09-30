79c80b9e4e39e69c3689bd9b73779eac94b8025b
remi <remi.vieux@gmail.com>
2025-09-30 16:27:48 +0200

    initial import

---
+++ readme.txt
@@ -0,0 +1,6 @@
+Ce script permet de transformer un HTML en PDF en utilisant chrome pour le rendu.
+Les images si passées par URL sur le web seront accessibles dans le PDF
+Les CSS sont bien pris en compte (de meme peuvent être inclus si accessibles depuis le web).
+
+Pour le deploiement, je vais utiliser le free tier Render.com
+
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const SECRET = process.env.BEARER_TOKEN
+
+app.use(bodyParser.json({limit: '5mb'})); // support HTML volumineux
+
+app.post('/generate-pdf', async (req, res) => {
+    const auth = req.headers.authorization;
+    if (!auth || auth !== `Bearer ${SECRET}`) {
+        return res.status(401).send({error: "Unauthorized"});
+    }
+    const {html} = req.body;
+    if (!html) return res.status(400).send({error: "Missing HTML"});
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setContent(html, {waitUntil: 'networkidle0'});
+        const pdfBuffer = await page.pdf({format: 'A4'});
+        await browser.close();
+
+        res.contentType('application/pdf');
+        res.send(pdfBuffer);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({error: err.message});
+    }
+});
+
+app.listen(PORT, () => console.log(`PDF service running on http://localhost:${PORT}`));
